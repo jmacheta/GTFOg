8433fd9399aef61c3e08dfcd0f8d77bbffb623d9
Jan Macheta <2372096+jmacheta@users.noreply.github.com>
2024-12-05 20:58:55 +0100

    working usb with SDK 2.8.0

--- software/firmware/requirements.cmake
+++ software/firmware/requirements.cmake
@@ -5,7 +5,7 @@ FetchContent_Declare(sml URL https://github.com/boost-ext/sml/archive/refs/tags/
 FetchContent_Declare(si URL https://github.com/bernedom/SI/archive/refs/tags/2.5.1.zip EXCLUDE_FROM_ALL)
 
 
-set(FMT_INSTALL OFF)
-set(FMT_OS OFF)
-set(FMT_UNICODE OFF)
+set(FMT_INSTALL OFF CACHE BOOL "")
+set(FMT_OS OFF CACHE BOOL "")
+set(FMT_UNICODE OFF CACHE BOOL "")
 FetchContent_Declare(fmt URL https://github.com/fmtlib/fmt/releases/download/11.0.2/fmt-11.0.2.zip EXCLUDE_FROM_ALL)
